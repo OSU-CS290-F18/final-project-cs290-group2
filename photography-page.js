2dd954d3f6962249e34ba02a29dde0f44c707809
roseg <rosegarcia4500@gmail.com>
2018-11-18 15:25:04 -0800

    Replace hover effect with click/modal.
    Hovering was messy - changed to modal with photo on click. Modal image
    src and caption is changed based on which image is clicked.

---
+++ photography-page.js
@@ -0,0 +1,23 @@
+var photoElements = document.getElementsByClassName('photo-container');
+var photoModal = document.getElementById('photo-modal');
+var closeModalButton = document.getElementById('close-photo-button');
+
+for(var x = 0; x < photoElements.length; x++){
+    photoElements[x].addEventListener('click', openPhotoModal);
+}
+
+closeModalButton.addEventListener('click', closePhotoModal);
+
+function openPhotoModal(){
+    var childNodes = this.children;
+    console.log(this.className);
+    photoModal.style.display = "block";
+    document.getElementById('modal-photo-link').src =
+        childNodes[0].src;
+    document.getElementById('modal-photo-cap').innerText =
+        childNodes[1].innerText;
+}
+
+function closePhotoModal(){
+    photoModal.style.display = "none";
+}
