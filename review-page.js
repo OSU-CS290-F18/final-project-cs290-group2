ea29226089a9246e76d7091e05b6a0d042a0f074
Anna Mollere <mollerea@oregonstate.edu>
2018-12-01 14:24:04 -0800

    adding javascript functionality to review page

---
+++ review-page.js
@@ -0,0 +1,6 @@
+
+
+var applyChangesButton = document.getElementById('update-button');
+applyChangesButton.addEventListener('click', function (){
+    console.log(document.getElementById('sort-date').value);
+});
