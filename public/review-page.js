595de6130b2eff03dab8b095fc7077ec1655759b
Anna Mollere <mollerea@oregonstate.edu>
2018-12-02 20:46:13 -0800

    added js functionality for adding a review post

--- public/review-page.js
+++ public/review-page.js
@@ -1,3 +1,11 @@
+
+
+/*
+ *
+ * Lines 8 - 90 == JS functionality for filter-bar
+ * 
+*/
+
 function filterReviews(yearInput, reviewerInput, keywordInput) {
     var reviewArr = document.querySelectorAll(".review");
     var reviewContainer = document.getElementById("review-container");
@@ -81,3 +89,119 @@ function getFilterChanges() {
 
 var applyChangesButton = document.getElementById("update-button");
 applyChangesButton.addEventListener("click", getFilterChanges);
+
+/*
+ *
+ * Lines 97- 
+ * 
+*/
+function clearReviewFields() {
+    document.getElementById("name-field").value = "";
+    document.getElementById("icon-field").value = "";
+    document.getElementById("review-field").value = "";
+    document.getElementById("rate-one").checked = false;
+    document.getElementById("rate-two").checked = false;
+    document.getElementById("rate-three").checked = false;
+    document.getElementById("rate-four").checked = false;
+    document.getElementById("rate-five").checked = false;
+}
+
+function createReview(usernameInput, iconURLInput, textInput, yearPosted) {
+    var reviewContainer = document.getElementById("review-container");
+
+    
+    var rateOne = document.getElementById('rate-one');
+    var rateTwo = document.getElementById('rate-two');
+    var rateThree = document.getElementById('rate-three');
+    var rateFour = document.getElementById('rate-four');
+    var rateFive = document.getElementById('rate-five'); 
+    var rateOptionArr = [rateOne, rateTwo, rateThree, rateFour, rateFive];
+    var rateInput = 0;
+    for(var i = 0; i < (rateOptionArr.length); i++) {
+        if(rateOptionArr[i].checked === true) {
+            rateInput = rateOptionArr[i].value;
+        }
+    }
+    console.log(rateInput);
+    if(usernameInput === "") {
+        usernameInput = "Anonymous"
+    }   
+    if(iconURLInput === "") {
+        iconURLInput = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStNsFRsJIPLrA-EgsJ0SE6BRFiy6v8-kGIbpiZzYAxm6dPJtny2w";
+    }
+    if((textInput === "") || (rateInput === 0)) {
+        alert("Review text and paw-rating required to leave a review.");
+    }
+    else{
+        var reviewDiv = document.createElement("div");
+        reviewDiv.classList.add("review");
+
+        var headerDiv = document.createElement("div");
+        headerDiv.classList.add("review-header");
+        reviewDiv.appendChild(headerDiv);
+
+        var userIconIMG = document.createElement("img");
+        userIconIMG.classList.add("user-icon");
+        userIconIMG.src = iconURLInput;
+        headerDiv.appendChild(userIconIMG);
+
+        var postedBy = document.createElement("span");
+        postedBy.classList.add("text");
+        postedBy.textContent = "Posted By: ";
+        var user = document.createElement("span");
+        user.classList.add("user");
+        user.textContent = usernameInput;
+        reviewDiv.setAttribute("data-user", usernameInput);
+        postedBy.appendChild(user);
+        headerDiv.appendChild(postedBy);
+
+
+        var pawDiv = document.createElement("div");
+        pawDiv.classList.add("paws");
+        var pawOne = document.createElement("span");
+        pawOne.textContent = "X";
+        pawDiv.appendChild(pawOne);
+        var pawTwo = document.createElement("span");
+        pawTwo.textContent = "X";
+        pawDiv.appendChild(pawTwo);
+        var pawThree = document.createElement("span");
+        pawThree.textContent = "X";
+        pawDiv.appendChild(pawThree);
+        var pawFour = document.createElement("span");
+        pawFour.textContent = "X";
+        pawDiv.appendChild(pawFour);
+        var pawFive = document.createElement("span");
+        pawFive.textContent = "X";
+        pawDiv.appendChild(pawFive);
+
+        var pawArr = [pawOne, pawTwo, pawThree, pawFour, pawFive];
+        for(var i = 0; i < rateInput; i++) {
+            pawArr[i].classList.add("checked");
+        }
+
+        reviewDiv.setAttribute('data-rating', rateInput);
+        headerDiv.appendChild(pawDiv);
+
+        var reviewBody = document.createElement("p");
+        reviewBody.classList.add("review-body");
+        reviewBody.textContent = textInput;
+        reviewDiv.appendChild(reviewBody);
+
+        reviewContainer.appendChild(reviewDiv);
+
+        clearReviewFields();
+    }
+}
+
+function getReviewInput() {
+    var usernameInput = document.getElementById("name-field").value;
+    var iconURLInput = document.getElementById("icon-field").value;
+    var textInput = document.getElementById("review-field").value;
+
+    var date = new Date();
+    var yearPosted = date.getFullYear();
+    createReview(usernameInput, iconURLInput, textInput, yearPosted); 
+}
+
+var submitReviewButton = document.getElementById("submit-review");
+submitReviewButton.addEventListener("click", getReviewInput);
