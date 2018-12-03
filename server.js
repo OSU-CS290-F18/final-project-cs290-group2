5507578b8a66fe19b7b9d37b343d37210b726333
roseg <rosegarcia4500@gmail.com>
2018-12-03 12:53:19 -0800

    adding primary mongo connection in server

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-//var logger = require('./logger');
+/*
+var MongoClient = require('mongodb').MongoClient;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoURL =
+	'mongodb://' + mongoUser + ':' + mongoPassword + '@' +
+	mongoHost + ':' + mongoPort + '/' + mongoDBName;
+*/
 
 var app = express();
 app.engine('handlebars', exphbs());
@@ -48,3 +60,13 @@ app.listen(port, function (err) {
     console.log("== Server listening on port ", port);
 });
 
+/*
+MongoClient.connect(mongoURL, function (err, client) {
+  if (err) {
+    throw err;
+  }
+  db = mongoDBDatabase = client.db(mongoDBName);
+  app.listen(3000, function () {
+    console.log("== Server listening on port 3000");
+  });
+});*/
